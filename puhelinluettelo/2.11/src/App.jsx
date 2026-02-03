0424a91801e51f03ae47375a92e68896bb061fda
Jarttu <167183980+Jarttu@users.noreply.github.com>
2026-02-03 11:16:26 +0200

    Add files via upload

---
+++ puhelinluettelo/2.11/src/App.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  	const [persons, setPersons] = useState([]) 
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response => {
+				setPersons(response.data)
+			})
+	}, [])
+
+  	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
+	const [filter, setFilter] = useState('')
+
+  	const handleNameChange = (event) => {
+    	setNewName(event.target.value)
+  	}
+
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value)
+	}
+
+	const handleFilterChange = (event) => {
+		setFilter(event.target.value)
+	}
+
+	const addPerson = (event) => {
+		event.preventDefault()
+
+		const personObj = {
+			name: newName,
+			number: newNumber
+		}
+
+		const duplicate = persons.some(person => person.name === newName)
+
+		if (duplicate) {
+			alert(`${newName} is already added to phonebook`)
+			return
+		}
+
+		setPersons(persons.concat(personObj))
+		setNewName('')
+		setNewNumber('')
+	}
+	
+	const showFiltered = persons.filter(person => 
+		person.name.toLowerCase().includes(filter.toLowerCase())
+	)
+	
+
+
+	return (
+    	<div>
+      		<h2>Phonebook</h2>
+			<div>
+				Filter shown with: <input 
+					value={filter} 
+					onChange={handleFilterChange}
+				/>
+			</div>
+			<h2>add a new person</h2>
+      		<form onSubmit={addPerson}>
+        		<div>
+          			name: <input value={newName} onChange={handleNameChange}/>
+        		</div>
+				<div>
+					Number: <input value={newNumber} onChange={handleNumberChange}/>
+				</div>
+        		<div>
+          			<button type="submit">add</button>
+        		</div>
+      		</form>
+      		<h2>Numbers</h2>
+      
+      		<ul>
+        		{showFiltered.map(person => 
+          			<li key={person.name}>{person.name} {person.number}</li>  
+        		)}
+      		</ul>
+    	</div>
+  	)
+
+}
+
+export default App
---
+++ puhelinluettelo/2.8/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+
+const App = () => {
+  	const [persons, setPersons] = useState([
+    	{ name: 'Arto Hellas', number: '040-123456' },
+  	]) 
+  	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
+
+  	const handleNameChange = (event) => {
+    	setNewName(event.target.value)
+  	}
+
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value)
+	}
+
+	const addPerson = (event) => {
+		event.preventDefault()
+
+		const person = {
+			name: newName,
+			number: newNumber
+		}
+
+		const duplicate = persons.some(person => person.name === newName)
+
+		if (duplicate) {
+			alert(`${newName} is already added to phonebook`)
+			return
+		}
+
+		setPersons(persons.concat(person))
+		setNewName('')
+		setNewNumber('')
+	}
+	
+	
+	
+
+
+	return (
+    	<div>
+      		<h2>Phonebook</h2>
+      		<form onSubmit={addPerson}>
+        		<div>
+          			name: <input value={newName} onChange={handleNameChange}/>
+        		</div>
+				<div>
+					Number: <input value={newNumber} onChange={handleNumberChange}/>
+				</div>
+        		<div>
+          			<button type="submit">add</button>
+        		</div>
+      		</form>
+      		<h2>Numbers</h2>
+      
+      		<ul>
+        		{persons.map(person => 
+          			<li key={person.name}>{person.name} {person.number}</li>  
+        		)}
+      		</ul>
+    	</div>
+  	)
+
+}
+
+export default App
